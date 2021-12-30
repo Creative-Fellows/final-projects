3ff979639381958985ef1ff54c9ff3aebb544b75
NourSafta <nour.safta@medtech.tn>
2021-12-30 22:28:45 +0100

    Part 4 Complete

--- hacker-news/client/src/App.js
+++ hacker-news/client/src/App.js
@@ -1,31 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
-import Stories from "./components/Stories"
-import Authors from './components/Authors';
-
+import logo from "./logo.svg";
+import "./App.css";
+import Stories from "./components/Stories";
+import Authors from "./components/Authors";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Search from "./components/Search";
+import Test from "./components/Test";
 
 function App() {
   return (
-    <div class="header">
-       <Router>
-      <nav>
-      <ul className="liste">
+    <div className="App">
+      <Router>
+        {/* <nav>
+          <Link to="/">Stories</Link>
+          <Link to="/authors">Authors</Link>
+        </nav> */}
+        <nav>
+          <ul className="liste">
             <Link to="/">
               <li className="items">TOP 10 Stories</li>
             </Link>
-            &nbsp;
             <Link to="/authors">
               <li className="items">TOP 10 Authors</li>
             </Link>
+            <Link to="/search">
+              <li className="items">Seach by Author</li>
+            </Link>
           </ul>
-      </nav>
-      <Routes>
+        </nav>
+        <Routes>
           <Route path="/" element={<Stories />} />
           <Route path="/authors" element={<Authors />} />
+          <Route path="/search" element={<Search />} />
         </Routes>
-        </Router>
-     </div>
+      </Router>
+      {/* <Stories /> */}
+      {/* <Search /> */}
+    </div>
   );
 }
 

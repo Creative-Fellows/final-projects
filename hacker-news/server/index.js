fc7b5ccf335a8a966a19f42a51f409cb6954d8ef
Mohamed Achraf Liratni <Mohamedachraf.liratni@medtech.tn>
2021-12-30 13:52:41 +0100

    part 2 is complete

--- hacker-news/server/index.js
+++ hacker-news/server/index.js
@@ -1,12 +1,18 @@
 const express = require("express")
 const dbconnection = require("./db/index");
+const Story = require("./models/Story");
 const storiesroute = require("./routes/stories.routes");
+const authorsroute = require("./routes/authors.routes");
 const cors = require("cors");
 const app = express()
+const Author = require("./models/Author");
+const port = 8000;
 
 app.use(express.json());
 app.use(cors());
 dbconnection();
 
 app.use("/", storiesroute);
+app.use("/", authorsroute);
+
 app.listen(8000, ()=>{console.log("serveur running")})

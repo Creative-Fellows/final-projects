25043587be0b6d0da79674f25463016def3b2c48
NourSafta <nour.safta@medtech.tn>
2021-12-30 16:14:45 +0100

    Part 3 Complete

--- hacker-news/client/src/components/Stories.js
+++ hacker-news/client/src/components/Stories.js
@@ -1,4 +1,4 @@
-import "./Stories.css"
+/*import "./Stories.css"
 import Axios from 'axios';
 import React,{useState,useEffect} from "react";
 
@@ -44,4 +44,36 @@ import React,{useState,useEffect} from "react";
     );
   }
   
-  export default Stories;
+  export default Stories;*/
+  import React, { useState, useEffect } from "react";
+  import Axios from "axios";
+  
+  export default function Stories() {
+    const [listOfUsers, setListOfUsers] = useState([]);
+  
+    useEffect(() => {
+      Axios.get("http://localhost:8000/api/stories").then((response) => {
+        setListOfUsers(response.data);
+      });
+    }, []);
+  
+    return (
+      <div>
+        <h1>Hacked News</h1>
+        <h5>Top 10 Stories</h5>
+  
+        <div className="aParent">
+          <span className="title">Title</span>
+          {listOfUsers.map((user) => {
+            return <p className="nowrap">{user.title}</p>;
+          })}
+        </div>
+        <div>
+          <span className="author">Author</span>
+          {listOfUsers.map((user) => {
+            return <p className="nowrap">{user.by}</p>;
+          })}
+        </div>
+      </div>
+    );
+  } 
